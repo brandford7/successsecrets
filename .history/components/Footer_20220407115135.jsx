d566a52940b176e59558703be32e628202a9ac64
brandford7 <brandfordjunior@gmail.com>
2022-04-07 11:55:54 +0000

    testing formik forms

---
+++ .history/components/Footer_20220407115135.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Box, chakra, Text, Stack, Img,Link } from "@chakra-ui/react";
+import MainLogo from "./MainLogo";
+
+const Footer = () => {
+  return (
+    <>
+     {} <chakra.footer bg="#23272e" p="10" h="200px">
+        <Stack direction="column" justify="center" align="center">
+          <MainLogo />
+          <Text rounded="full" color="white">
+            18+
+          </Text>
+          <Stack direction="row" spacing="5">
+            <Link href="youtube.com" isExternal>
+              <Img
+                src="/youtube.png"
+                objectFit="contain"
+                rounded="full"
+                h="10"
+              />
+            </Link>
+            <Link>
+              <Img
+                src="/telegram.png"
+                objectFit="contain"
+                rounded="full"
+                h="10"
+              />
+            </Link>
+          </Stack>
+        </Stack>
+      </chakra.footer>
+    </>
+  );
+};
+
+export default Footer;
--- .history/pages/_app_20220328114114.js
+++ .history/pages/_app_20220328114114.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Layout>
-        <Component {...pageProps} />
+        <Component {...pageProps} />;
       </Layout>
     </ChakraProvider>
   );
