5004c3c47854ad4e5d352aee1125aea199fedc55
brandford7 <brandfordjunior@gmail.com>
2022-05-02 21:22:37 +0000

    removal of suspense

--- .history/components/sections/TopSection_20220502211404.jsx
+++ .history/components/sections/TopSection_20220502211404.jsx
@@ -1,13 +1,13 @@
 import React,{lazy, Suspense} from "react";
 import { Box, Heading,  Stack } from "@chakra-ui/react";
-import Tricks from "./Tricks"
-//itconst Tricks = lazy(() => import("./Tricks"));
+/import Tricks from "./Tricks"
+const Tricks = lazy(() => import("./Tricks"));
 
 const TopSection = () => {
-//const Loader =<p>Loading...</p>
+const Loader =<p>Loading...</p>
   return (
     <>
-      {/* <Suspense fallback={Loader}>*/}
+       <Suspense fallback={Loader}>
         
         <Box
           w="100%"
@@ -26,7 +26,8 @@ const TopSection = () => {
             <Tricks />
           </Stack>
         </Box>
-      {/* </Suspense> */}
+      
+    </Suspense> 
     </>
   );
 };
