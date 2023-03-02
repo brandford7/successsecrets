d3f03ef34482dbfc628959b735cb6518fd80c689
brandford7 <brandfordjunior@gmail.com>
2023-03-02 11:33:37 +0000

    fixing url errors 1.7

--- .history/components/forms/UpdateMatchForm_20230302112118.jsx
+++ .history/components/forms/UpdateMatchForm_20230302112118.jsx
@@ -42,7 +42,7 @@ const UpdateMatchForm = ({ Values }) => {
           setTimeout(async () => {
             try {
               const res = await fetch(
-                `successsecretsbet.com/api/predictions/${id}`,
+                `successsecrets/api/predictions/${id}`,
                 {
                   method: "PUT",
                   headers: {
