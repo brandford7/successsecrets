0bd8e6f797a2e1a7cc995a05b0ef5ca93544c037
brandford7 <brandfordjunior@gmail.com>
2023-03-06 21:46:17 +0000

    refined mongodb query in europe page

--- .history/pages/predictions/europe_20230306212950.js
+++ .history/pages/predictions/europe_20230306212950.js
@@ -32,7 +32,7 @@ export async function getStaticProps() {
   await dbConnect();
 
   const predictions = await Prediction.find({
-    country: { $regex: "UEFA" },
+    country:{re},
     start_date: {
       $gte: startOfDay(new Date()),
       $lte: endOfDay(new Date()),
