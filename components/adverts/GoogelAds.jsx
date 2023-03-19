c21ed71a07ca6854d7ac6c52b5e55deabe8686d8
brandford7 <brandfordjunior@gmail.com>
2023-03-19 13:10:52 +0000

    new display ads

--- components/adverts/GoogelAds.jsx
+++ components/adverts/GoogelAds.jsx
@@ -1,19 +1,23 @@
 import { useEffect } from "react";
 
 const GoogleAds = () => {
+  const { currentPath } = props;
+
   useEffect(() => {
     (window.adsbygoogle = window.adsbygoogle || []).push({});
   }, []);
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: "block" }}
-      data-ad-client="ca-pub-3825617756167561"
-      data-ad-slot="1234567890"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    ></ins>
+    <div key={currentPath}>
+      <ins
+        className="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-3825617756167561"
+        data-ad-slot="1626852337"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </div>
   );
 };
 
--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -3,19 +3,19 @@ import { Box, Container } from "@chakra-ui/react";
 import Header from "../headers/Header";
 import Footer from "../footers/Footer";
 import ScrollToTop from "../footers/ScrollToTop";
-
+import GoogleAds from "../adverts/GoogelAds";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Box bg="gray.100" h="auto" p="0" minH="800px">
         <Container bg="bg.200" maxW="container.xl" textAlign="center" p="0">
-          <div id="ezoic-pub-ad-placeholder-103"> </div>
+          <GoogleAds />
           <Header />
 
           <main>{children}</main>
-         
-          <div id="ezoic-pub-ad-placeholder-102"> </div>
+
+          <GoogleAds />
           <Footer />
         </Container>
 
