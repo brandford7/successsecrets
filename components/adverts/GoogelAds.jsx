16a9b4213eb6b6f9a625b14a2af9c93857b40568
brandford7 <brandfordjunior@gmail.com>
2023-03-20 14:50:57 +0000

    addition of new sitemap and robots.txt

--- components/adverts/GoogelAds.jsx
+++ components/adverts/GoogelAds.jsx
@@ -1,14 +1,14 @@
 import { useEffect } from "react";
 
-const GoogleAds = ({props}) => {
- // const { currentPath } = props;
+const GoogleAds = ({...props}) => {
+ const { currentPath } = props;
 
   useEffect(() => {
     (window.adsbygoogle = window.adsbygoogle || []).push({});
-  }, []);
+  }, [currentPath]);
 
   return (
-    <div >
+    <div key={currentPath} >
       <ins
         className="adsbygoogle"
         style={{ display: 'block' }}
---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: "https://successsecretsbet.com",
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+ 
+ 
+ 
+};
