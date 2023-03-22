7f3fbf87209574710ca8e82a7ce0b25ced604006
brandford7 <brandfordjunior@gmail.com>
2023-03-22 17:17:35 +0000

    remove manual display ads

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -3,18 +3,16 @@ import { Box, Container } from "@chakra-ui/react";
 import Header from "../headers/Header";
 import Footer from "../footers/Footer";
 import ScrollToTop from "../footers/ScrollToTop";
-import GoogleAds from "../adverts/GoogelAds";
+//import GoogleAds from "../adverts/GoogelAds";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Box bg="gray.100" h="auto" p="0" minH="800px">
         <Container bg="bg.200" maxW="container.xl" textAlign="center" p="0">
-          
           <Header />
-
           <main>{children}</main>
-          <GoogleAds />
+          {/*<GoogleAds />*/}
           <Footer />
         </Container>
 
