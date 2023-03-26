e40c5d0cdcf4de37aedd541cc12025344282412e
brandford7 <brandfordjunior@gmail.com>
2023-03-26 21:46:09 +0000

    reducing meta description length in home page

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ predictions }) {
         />
         <meta
           name="description"
-          content="Success Secrets Bet is the ultimate destination for soccer enthusiasts looking to stay ahead of the game with prediction on football. Our website offers comprehensive soccer bet predictions and analysis to help our users make informed betting decisions. Predictions from The best site for football predictions. English Premier League, La Liga, Ligue 1, Serie A, FA Cup League Cup, UEFA Champions League, Europa League Conference League CAF Champions League, UEFA
+          content="Success Secrets Bet is the ultimate destination for soccer enthusiasts looking to stay ahead of the game with prediction on football. Our website offers comprehensive soccer bet predictions and analysis for the  English Premier League, La Liga, Ligue 1, Serie A, FA Cup League Cup, UEFA Champions League, Europa League Conference League CAF Champions League, UEFA
           Nations League, World Cup, European ChampionShip,Premier League, Serie A , La Liga, Bundesliga, Ligue1 , Eredivisie, Portuguese League, and more."
         />
 
