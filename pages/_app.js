659e0478049f958c52879a64ede3abbfc0fd6033
brandford7 <brandfordjunior@gmail.com>
2023-03-26 21:57:12 +0000

    implementation of vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ import Script from "next/script";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import * as gtag from "../lib/gtag";
+import { Analytics } from "@vercel/analytics/react";
 
 
 
@@ -82,12 +83,16 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
           `,
               }}
             />
-            <Script  id="show-ad" async src="https://fundingchoicesmessages.google.com/i/pub-3825617756167561?ers=1" nonce="55nt8l_NwNSXsr9GjA2oMw"
+            <Script
+              id="show-ad"
+              async
+              src="https://fundingchoicesmessages.google.com/i/pub-3825617756167561?ers=1"
+              nonce="55nt8l_NwNSXsr9GjA2oMw"
               dangerouslySetInnerHTML={{
                 __html: `(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();`,
               }}
             />
-            
+
             <Script
               async
               src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3825617756167561"
@@ -96,6 +101,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 
             <Layout>
               <Component {...pageProps} />
+              <Analytics />
             </Layout>
           </ChakraProvider>
         </SessionProvider>
